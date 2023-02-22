ba5af348fea587f6fbb55ff1c1a22cd70cbaa39b
jwc20 <choij2015@gmail.com>
2023-02-21 17:44:37 -0800

    FIX

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
