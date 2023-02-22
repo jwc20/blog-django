14627f1545497fbaacfe8111e6250a5118c6a616
jwc20 <choij2015@gmail.com>
2023-02-21 16:03:00 -0800

    Modified settings to add token authentication.

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "posts.apps.PostsConfig",
+    "rest_framework.authtoken",
+    "posts",
 ]
 
 REST_FRAMEWORK = {
