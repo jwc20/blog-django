a41aa422b50a4606051c09deb166553ed4271e1b
jwc20 <choij2015@gmail.com>
2023-02-21 16:01:43 -0800

    Changed Authentication to Token Authentication.

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
